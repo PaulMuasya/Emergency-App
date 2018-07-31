5b5d86817adde0280b83d6529db4f1b0bf96ce36
PaulMuasya <32673540+PaulMuasya@users.noreply.github.com>
2018-07-31 19:41:42 +0300

    Update settings.py

--- Emergency/settings.py
+++ Emergency/settings.py
@@ -32,7 +32,7 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 
-#import os
+import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
