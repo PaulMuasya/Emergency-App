7ae704e596072b0c11ed79926bb262f14fd2de86
PaulMuasya <32673540+PaulMuasya@users.noreply.github.com>
2018-07-31 19:11:05 +0300

    Update settings.py

--- Emergency/settings.py
+++ Emergency/settings.py
@@ -11,13 +11,12 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 import dj_database_url
 
-...
 
 DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
-...
+
 
 DATABASES = {
     'default': {
